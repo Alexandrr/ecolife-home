a779549a9326f5cde62569d083f792ba57727ffc
ba1ROn <glavandimoncik@gmail.com>
2024-10-02 14:36:35 +0300

    changed btn

--- ecolife-home/src/Header.js
+++ ecolife-home/src/Header.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './Header.css'; 
-import logo3 from './photos/logo3.jpg'; 
-import logo2 from './photos/logo2.jpg'; 
+import logo3 from './photos/logo3.jpg';  // Большой логотип
+import logo2 from './photos/logo2.jpg';  // Маленький логотип
+
 function Header() {
   return (
     <header className="header">
-
       <div className="logo">
         <img src={logo3} alt="Ecolife" />
       </div>
 
       <div className="right-section">
+        <div className="profile-icon">
+          <img src={logo2} alt="Small logo" />
+        </div>
         <div className="language-switcher">
           <button>RU</button>
           <button>AZ</button>
-        </div>
-        <div className="profile-icon">
-          <img src={logo2} alt="Profile" />
         </div>
       </div>
     </header>
