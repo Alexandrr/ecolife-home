cf6407538324e06a0a46856754d24722d9c69683
ba1ROn <glavandimoncik@gmail.com>
2024-10-02 18:14:34 +0300

    fix mistake

--- ecolife-home/src/App.js
+++ ecolife-home/src/App.js
@@ -3,41 +3,38 @@ import Header from './Header';
 import './App.css';
 
 function App() {
-  const [isListVisible, setIsListVisible] = useState(false);
+  const [isCodeVisible, setIsCodeVisible] = useState(false);
 
-  const handleSearchClick = () => {
-    setIsListVisible(true); // покащывать кнопку
+  const handleMenuClick = (type) => {
+    if (type === 'code') {
+      setIsCodeVisible(true);
+    } else {
+      setIsCodeVisible(false);
+    }
   };
 
   return (
     <div className="app">
       <Header />
 
-      <div className="search-section">
-        <label htmlFor="searchInput">Введите код:</label>
-        <input type="text" id="searchInput" placeholder="Текстовое поле для поиска" />
-        <button className="search-button" onClick={handleSearchClick}>ПОИСК</button>
-      </div>
-
-      <div className="horizontal-line"></div>
-
       <div className="content">
-        <div className="left-sidebar"></div>
+        <div className="left-sidebar">
+          <h3>Меню</h3>
+          <button className="menu-button" onClick={() => handleMenuClick('product')}>Подбор товара</button>
+          <button className="menu-button" onClick={() => handleMenuClick('code')}>По коду</button>
+        </div>
 
         <div className="main-content">
-          {isListVisible && (
-            <ul className="search-results">
-              <li><strong>Код:</strong> —</li>
-              <li><strong>Артикул:</strong> —</li>
-              <li><strong>Наименование:</strong> —</li>
-              <li><strong>Полное наименование:</strong> —</li>
-              <li><strong>Розничная цена:</strong> —</li>
-              <li><strong>Количество на складах:</strong> —</li>
-            </ul>
+          {isCodeVisible && (
+            <div className="search-section">
+              <label htmlFor="searchInput">Введите код:</label>
+              <input type="text" id="searchInput" placeholder="Текстовое поле для поиска" />
+              <button className="search-button">ПОИСК</button>
+            </div>
           )}
         </div>
 
-        <div className="right-sidebar"></div>
+        <div className="right-sidebar"></div> 
       </div>
     </div>
   );
