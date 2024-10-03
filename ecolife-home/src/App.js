9bb7f36800c5b6a0401c1b219aa29c1e2ec5621d
ba1ROn <glavandimoncik@gmail.com>
2024-10-03 17:55:42 +0300

    new menu

--- ecolife-home/src/App.js
+++ ecolife-home/src/App.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import Header from './Header';
+import { Menu } from 'antd';  
+import { MailOutlined } from '@ant-design/icons';
 import './App.css';
 
-function App() {
-  const [isCodeVisible, setIsCodeVisible] = useState(false);
+const { SubMenu } = Menu;
 
-  const handleMenuClick = (type) => {
-    if (type === 'code') {
-      setIsCodeVisible(true);
-    } else {
-      setIsCodeVisible(false);
-    }
+function App() {
+  const [activeMenu, setActiveMenu] = useState(null);
+
+  const handleMenuClick = (e) => {
+    setActiveMenu(e.key);  // Определяем, какая опция была выбрана
   };
 
   return (
@@ -19,22 +19,38 @@ function App() {
 
       <div className="content">
         <div className="left-sidebar">
-          <h3>Меню</h3>
-          <button className="menu-button" onClick={() => handleMenuClick('product')}>Подбор товара</button>
-          <button className="menu-button" onClick={() => handleMenuClick('code')}>По коду</button>
+          <Menu
+            mode="inline"
+            style={{ width: 256 }}
+            onClick={handleMenuClick}
+          >
+            <SubMenu key="sub1"  title="Меню">
+              <Menu.ItemGroup key="g1">
+                <Menu.Item key="1">Подбор товара</Menu.Item>
+                <Menu.Item key="2">По коду</Menu.Item>
+                <Menu.Item key="3">Избранное</Menu.Item>
+                <Menu.Item key="4">История</Menu.Item>
+              </Menu.ItemGroup>
+            </SubMenu>
+          </Menu>
         </div>
 
         <div className="main-content">
-          {isCodeVisible && (
+          {activeMenu === '2' && (
             <div className="search-section">
               <label htmlFor="searchInput">Введите код:</label>
-              <input type="text" id="searchInput" placeholder="Текстовое поле для поиска" />
+              <input
+                type="text"
+                id="searchInput"
+                placeholder="Текстовое поле для поиска"
+                style={{ marginRight: '10px' }}
+              />
               <button className="search-button">ПОИСК</button>
             </div>
           )}
         </div>
 
-        <div className="right-sidebar"></div> 
+        <div className="right-sidebar"></div>
       </div>
     </div>
   );
