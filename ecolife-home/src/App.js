1820f3ca4c90b5c268dc51af8b8b05f0ea842ec4
ba1ROn <glavandimoncik@gmail.com>
2024-10-07 21:00:32 +0300

    change menu

--- ecolife-home/src/App.js
+++ ecolife-home/src/App.js
@@ -18,19 +18,19 @@ function App() {
       <Header />
 
       <div className="content">
-        <div className="left-sidebar">
+      <div className="left-sidebar">
           <Menu
             mode="inline"
             style={{ width: 256 }}
             onClick={handleMenuClick}
           >
-            <SubMenu key="sub1"  title="Меню">
-              <Menu.ItemGroup key="g1">
-                <Menu.Item key="1">Подбор товара</Menu.Item>
-                <Menu.Item key="2">По коду</Menu.Item>
-                <Menu.Item key="3">Избранное</Menu.Item>
-                <Menu.Item key="4">История</Menu.Item>
-              </Menu.ItemGroup>
+            <SubMenu key="sub1" title="Меню">
+              <Menu.Item key="5">История</Menu.Item>
+              <Menu.Item key="6">Избранное</Menu.Item>
+              <SubMenu key="sub2" title="Подбор товара">
+                <Menu.Item key="7">По коду</Menu.Item>
+                <Menu.Item key="8">По названию</Menu.Item>
+              </SubMenu>
             </SubMenu>
           </Menu>
         </div>
