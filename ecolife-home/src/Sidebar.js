6dd77bbeecf0aaa746db02ee1ec21f74d624fdde
ba1ROn <glavandimoncik@gmail.com>
2024-10-11 11:11:14 +0300

    menu #1

--- ecolife-home/src/Sidebar.js
+++ ecolife-home/src/Sidebar.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from 'antd';
+import { AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import './Sidebar.css';
 
-const { SubMenu } = Menu;
+const items = [
+  {
+    key: 'sub1',
+    label: 'Подбор товара',
+    children: [
+      {
+        key: '1',
+        label: 'По коду',
+      },
+      {
+        key: '2',
+        label: 'По названию',
+      },
+    ],
+  },
+  {
+    key: 'sub2',
+    label: 'История',
+    icon: <AppstoreOutlined />,
+  },
+  {
+    key: 'sub3',
+    label: 'Избранное',
+    icon: <SettingOutlined />,
+  },
+];
 
 function Sidebar({ handleMenuClick }) {
+  const [showInput, setShowInput] = useState(false);
+
+  const handleMenuSelect = ({ key }) => {
+    if (key === '1') {
+      setShowInput(true);
+    } else {
+      setShowInput(false);
+    }
+  };
+
   return (
-    <div className="left-sidebar">
+    <div className='Sidebar'>
       <Menu
         mode="inline"
         style={{ width: 256 }}
         onClick={handleMenuClick}
-      >
-        <SubMenu key="sub1" title="Меню">
-          <Menu.Item key="5">История</Menu.Item>
-          <Menu.Item key="6">Избранное</Menu.Item>
-          <SubMenu key="sub2" title="Подбор товара">
-            <Menu.Item key="7">По коду</Menu.Item>
-            <Menu.Item key="8">По названию</Menu.Item>
-          </SubMenu>
-        </SubMenu>
-      </Menu>
+        onSelect={handleMenuSelect}
+        defaultOpenKeys={['sub1']}
+        items={items}
+      />
+
+      {showInput && (
+        <div className="search-section">
+          <label htmlFor="searchInput">Введите код:</label>
+          <input
+            type="text"
+            id="searchInput"
+            placeholder="Введите код товара"
+            style={{ marginTop: '10px', width: '100%' }}
+          />
+        </div>
+      )}
     </div>
   );
 }
