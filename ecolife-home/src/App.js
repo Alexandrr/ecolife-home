dd60487c5ee1482f2f12a7b6cddd8a1632064602
ba1ROn <glavandimoncik@gmail.com>
2024-10-15 21:33:02 +0300

    when pressing btn

--- ecolife-home/src/App.js
+++ ecolife-home/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState} from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
@@ -10,19 +10,7 @@ function App() {
   const [breadcrumbItems, setBreadcrumbItems] = useState(['Home']);
   const [showInput, setShowInput] = useState(false);
 
-  useEffect(() => {                                                  
-    fetch('http://localhost:5000/api/GetData/databycode', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({lang : 'string' , code:'string' , searchCriteria:'string'}),})
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Ошибка:', error));  // вывод ошибок
-  }, []);
-
-
+  
   const handleMenuSelect = (key, name) => {
     setShowInput(key === '1'); 
     setBreadcrumbItems(['Home', name]);  // Обновляем хлебные крошки как массив
--- ecolife-home/src/Content.js
+++ ecolife-home/src/Content.js
@@ -2,6 +2,20 @@ import React from 'react';
 import './Content.css';
 
 function Content({ showInput }) {
+
+  const handleSearch = () => {
+    fetch('http://localhost:5000/api/GetData/databycode', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ lang: "string", code: "02265", searchCriterias: "string" }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Ошибка:', error));
+  };
+
   return (
     <div className="main-content">
       {showInput && (
@@ -13,7 +27,7 @@ function Content({ showInput }) {
             placeholder="Введите код товара"
             style={{ marginRight: '10px' }}
           />
-          <button className="search-button">ПОИСК</button>
+          <button className="search-button" onClick={handleSearch}>ПОИСК</button>
         </div>
       )}
     </div>
